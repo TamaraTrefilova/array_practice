6a4b69f832d8c388b8da9bf1a04d1fee4674a808
Trevor Teigen and Tamara Trefilova <trevor@>
2019-06-10 10:14:45 -0700

    added a new loop

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ var color;
 var food;
 var animal;
 
-var array =[];
+var arrays =[];
 
 $(function(){
   $("form").submit(function(event) {
@@ -10,14 +10,12 @@ $(function(){
     color = $("input#color").val();
     food = $("input#food").val();
     animal = $("input#animal").val();
-    array.push(color);
-    array.push(food);
-    array.push(animal);
-    $(".colorInput").text(array[0]);
-    $(".foodInput").text(array[1]);
-    $(".animalInput").text(array[2]);
+    arrays.push(color);
+    arrays.push(food);
+    arrays.push(animal);
+    arrays.forEach(function(array){
+      $(".list").append("<li>"+array+"</li>");
+    });
     $(".result").show();
-
-
   });
 });
